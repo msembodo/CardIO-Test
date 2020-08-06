721d9f0c4ea89117ae730b5eff5d8c8456cfc8ed
gifarmuhammad <gifarmuhammadihsan@gmail.com>
2020-08-06 09:52:59 +0700

    Create RFM ISIM model & create RFM ISIM fxid

--- src/main/java/com/idemia/tec/jkt/cardiotest/controller/CardiotestController.java
+++ src/main/java/com/idemia/tec/jkt/cardiotest/controller/CardiotestController.java
@@ -350,6 +350,76 @@ public class CardiotestController {
     @FXML
     private TextField txtRfmUsimCustomKid;
 
+
+    // RFM ISIM tab
+    @FXML
+    private CheckBox chkIncludeRfmIsim;
+    @FXML
+    private CheckBox chkIncludeRfmIsimUpdateRecord;
+    @FXML
+    private CheckBox chkIncludeRfmIsimExpandedMode;
+    @FXML
+    private TextField txtRfmIsimMslByte;
+    @FXML
+    private ComboBox<String> cmbRfmIsimCipherAlgo;
+    @FXML
+    private CheckBox chkRfmIsimUseCipher;
+    @FXML
+    private ComboBox<String> cmbRfmIsimAuthVerif;
+    @FXML
+    private ComboBox<String> cmbRfmIsimSigningAlgo;
+    @FXML
+    private ComboBox<String> cmbRfmIsimPorRequirement;
+    @FXML
+    private ComboBox<String> cmbRfmIsimPorSecurity;
+    @FXML
+    private CheckBox chkRfmIsimCipherPor;
+    @FXML
+    private ComboBox<String> cmbRfmIsimCounterCheck;
+    @FXML
+    private TextField txtRfmIsimTar;
+    @FXML
+    private TextField txtRfmIsimTargetEf;
+    @FXML
+    private TextField txtRfmIsimTargetEfBadCase;
+    @FXML
+    private CheckBox chkRfmIsimFullAccess;
+    @FXML
+    private Label lblRfmIsimCustomTarget;
+    @FXML
+    private ComboBox<String> cmbRfmIsimCustomTargetAcc;
+    @FXML
+    private TextField txtRfmIsimCustomTargetEf;
+    @FXML
+    private Label lblRfmIsimCustomTargetBadCase;
+    @FXML
+    private ComboBox<String> cmbRfmIsimCustomTargetAccBadCase;
+    @FXML
+    private TextField txtRfmIsimCustomTargetEfBadCase;
+    @FXML
+    private CheckBox chkRfmIsimUseSpecificKeyset;
+    @FXML
+    private Label lblRfmIsimCipheringKeyset;
+    @FXML
+    private ComboBox<String> cmbRfmIsimCipheringKeyset;
+    @FXML
+    private Label lblRfmIsimKic;
+    @FXML
+    private CheckBox chkRfmIsimCustomKic;
+    @FXML
+    private TextField txtRfmIsimCustomKic;
+    @FXML
+    private Label lblRfmIsimAuthKeyset;
+    @FXML
+    private ComboBox<String> cmbRfmIsimAuthKeyset;
+    @FXML
+    private Label lblRfmIsimKid;
+    @FXML
+    private CheckBox chkRfmIsimCustomKid;
+    @FXML
+    private TextField txtRfmIsimCustomKid;
+
+
     // bottom tab pane
     @FXML
     private TabPane tabBottom;
